37f3e59ac819f7671c5ed93cc3d7ce47a44328a2
Mrinal Fouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2023-07-16 00:23:28 +0530

    feature add -- routing with button

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Login = () => {
   return (
     <div>
       Login
+      <Link href='/'>Go back to Home Page</Link>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
 "use client"
+import Link from 'next/link'
 import styles from './page.module.css'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
   return (
-    <main className={styles.main}>
-        <h1> Joy maa tara</h1>
-        <button onClick={()=> alert('hello')}>click on me</button>
+    <main >
+        <h1> Basic Routing | Make new Page</h1>
+        <Link href='/login'>Go to Login Page</Link>
+        <br/>
+        <Link href='/about'>Go to about page</Link>
+        <br/>
+        <br/>
+        <button onClick={()=> router.push('/login')}>Go to Login Page</button>
+        <button onClick={()=> router.push('/about')}>Go to About Page</button>
     </main>
   )
 }
