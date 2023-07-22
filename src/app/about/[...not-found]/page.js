4595a41d0c4f6bbf7e59a6e4f7acf7675cd90f44
Mrinal Fouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2023-07-23 01:19:46 +0530

    feature add -404 page custom

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return(
+        <div>
+            <h1>This about page is not available</h1>
+        </div>
+    )
+}
---
+++ src/app/study/[...lecture]/page.js
@@ -0,0 +1,9 @@
+"use client"
+export default function Lecture({params}){
+    console.log(params)
+    return(
+        <div>
+            <h1>Lectures 1</h1>
+        </div>
+    )
+ }
